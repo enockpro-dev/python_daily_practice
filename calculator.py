5dc727bc790f0404e349b21577bbb66f592e8b36
enockpro-dev <basistenoch@gmail.com>
2026-04-18 00:08:13 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,9 +1,10 @@
 num1 = float(input("Enter first number: "))
+
 num2 = float(input("Enter the second number: "))
 
-operation = input("Enter operation:" )
+OPERATION = input("Enter operation:" )
 
-if operation == "+":
+if OPERATION == "+":
     print("result:", num1 + num2)
 
 elif operation == "-":
