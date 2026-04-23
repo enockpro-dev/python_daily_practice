98c49084507a9e5bde8a321bfc2588274d5e9013
enockpro-dev <basistenoch@gmail.com>
2026-04-23 23:37:28 +0300

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,10 +1,11 @@
-num1 = float(input("Enter first number: "))
+import math
 
+num1 = float(input("Enter first number: "))
 num2 = float(input("Enter the second number: "))
 
-OPERATION = input("Enter operation:" )
+operation = input("Enter operation (+, -, *, /, log): ").strip().lower()
 
-if OPERATION == "+":
+if operation == "+":
     print("result:", num1 + num2)
 
 elif operation == "-":
@@ -14,7 +15,16 @@ elif operation == "*":
     print("result:", num1 * num2)
 
 elif operation == "/":
-    print("result:", num1 / num2)
+    if num2 == 0:
+        print("Cannot divide by zero")
+    else:
+        print("result:", num1 / num2)
+
+elif operation == "log":
+    if num1 <= 0 or num2 <= 0 or num2 == 1:
+        print("For log, the first number must be > 0 and the second number must be > 0 and not equal to 1")
+    else:
+        print("result:", math.log(num1, num2))
 
 else:
     print("Invalid operation entered")
